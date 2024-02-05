0e4188da284064e11f595e7a3e2286abbeac23a2
Ueverton-oliveira <ueverton.souz@gmail.com>
2024-02-05 10:44:40 -0400

    feat: model user

---
+++ user_authentication/spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :user do
+    email { "MyString" }
+    first_name { "MyString" }
+    last_name { "MyString" }
+    user_name { "MyString" }
+    password { "MyString" }
+  end
+end
